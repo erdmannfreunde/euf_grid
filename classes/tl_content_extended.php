8d782e4719fc03dda9592162bdd689fa10b95889
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-04-13 14:44:13 +0200

    Automatically create end element. Fixes #11.

--- classes/tl_content_extended.php
+++ classes/tl_content_extended.php
@@ -24,4 +24,50 @@ class tl_content_extended extends tl_content
     return $return;
   }
 
+    public function onsubmitCallback(\DataContainer $dc)
+    {
+        if (!in_array($dc->activeRecord->type, array('rowStart', 'colStart'))) {
+            return;
+        }
+
+        if ('auto' !== \Contao\Input::post('SUBMIT_TYPE') && $this->siblingStopElmentIsMissing(
+                $dc->activeRecord->pid,
+                $dc->activeRecord->ptable,
+                $dc->activeRecord->sorting,
+                substr($dc->activeRecord->type, 0, 3)
+            )) {
+            $data = $dc->activeRecord->row();
+            unset($data['id']);
+            $data['type']    = str_replace('Start', 'End', $dc->activeRecord->type);
+            $data['sorting'] += 32;
+
+            $newElement = new \Contao\ContentModel();
+            $newElement->setRow($data);
+            $newElement->save();
+        }
+    }
+
+    public function siblingStopElmentIsMissing($pid, $ptable, $sorting, $rowOrCol)
+    {
+        if (!in_array($rowOrCol, array('row', 'col'))) {
+            throw new InvalidArgumentException('Argument $rowOrCol must be either "row" or "col"');
+        }
+        $statement = \Contao\Database::getInstance()
+            ->prepare(
+                'SELECT * FROM tl_content WHERE pid=? AND ptable=? AND sorting>? AND type IN("' . $rowOrCol . 'Start", "'
+                . $rowOrCol . 'End") ORDER BY sorting'
+            )
+            ->limit(1)
+            ->execute($pid, $ptable, $sorting);
+
+        if (false === $row = $statement->fetchAssoc()) {
+            return true;
+        }
+
+        if ($rowOrCol . 'End' !== $row['type']) {
+            return true;
+        }
+
+        return false;
+    }
 }
