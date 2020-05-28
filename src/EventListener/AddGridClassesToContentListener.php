a7b7c71f2271119586401ca4d447c78079dce461
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-05-28 18:06:51 +0200

    Output the grid classes in the backend

--- src/EventListener/AddGridClassesToContentListener.php
+++ src/EventListener/AddGridClassesToContentListener.php
@@ -15,64 +15,45 @@ namespace ErdmannFreunde\ContaoGridBundle\EventListener;
 
 use Contao\ContentElement;
 use Contao\ContentModel;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Contao\StringUtil;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 final class AddGridClassesToContentListener
 {
+
+    private $requestStack;
+    private $scopeMatcher;
+
+    public function __construct(RequestStack $requestStack, ScopeMatcher $scopeMatcher)
+    {
+        $this->requestStack = $requestStack;
+        $this->scopeMatcher = $scopeMatcher;
+    }
+
     public function onGetContentElement(ContentModel $contentModel, string $strBuffer, ContentElement $element)
     {
-        // Init
         $strClasses = '';
 
         // Bei diesen ContentElementen soll nichts verändert werden
         $arrWrongCE = ['rowStart', 'rowEnd', 'colEnd'];
 
-        // Abfrage, ob anzupassenden CEs und Klassen gesetzt wurden
         if (!\in_array($contentModel->type, $arrWrongCE, true)
-            && (isset($contentModel->grid_columns)
-                || isset($contentModel->grid_options))) {
-            // Columns Klassen auslesen und in String speichern
+            && (isset($contentModel->grid_columns) || isset($contentModel->grid_options))) {
             if ($contentModel->grid_columns) {
-                $env            = 'FE';
-                $strField       = 'grid_columns';
-                $arrGridClasses = unserialize($contentModel->grid_columns);
+                $arrGridClasses = StringUtil::deserialize($contentModel->grid_columns);
                 foreach ($arrGridClasses as $class) {
-                    // HOOK: create and manipulate grid classes
-                    if (isset($GLOBALS['TL_HOOKS']['manipulateGridClasses'])
-                        && \is_array(
-                            $GLOBALS['TL_HOOKS']['manipulateGridClasses']
-                        )) {
-                        foreach ($GLOBALS['TL_HOOKS']['manipulateGridClasses'] as $callback) {
-                            $this->import($callback[0]);
-                            $class = $this->{$callback[0]}->{$callback[1]}($env, $strField, $class);
-                        }
-                    }
-
                     $strClasses .= $class.' ';
                 }
             }
 
-            // Weitere Optionen Klassen auslesen und in String speichern
             if ($contentModel->grid_options) {
-                $env            = 'FE';
-                $strField       = 'grid_options';
-                $arrGridClasses = unserialize($contentModel->grid_options);
+                $arrGridClasses = StringUtil::deserialize($contentModel->grid_options);
                 foreach ($arrGridClasses as $class) {
-                    // HOOK: create and manipulate grid classes
-                    if (isset($GLOBALS['TL_HOOKS']['manipulateGridClasses'])
-                        && \is_array(
-                            $GLOBALS['TL_HOOKS']['manipulateGridClasses']
-                        )) {
-                        foreach ($GLOBALS['TL_HOOKS']['manipulateGridClasses'] as $callback) {
-                            $this->import($callback[0]);
-                            $class = $this->{$callback[0]}->{$callback[1]}($env, $strField, $class);
-                        }
-                    }
-
                     $strClasses .= $class.' ';
                 }
             }
 
-            // Ausgabe abhängig vom Elementtyp anpassen
             switch ($contentModel->type) {
                 case 'rowStart':
                 case 'rowEnd':
@@ -80,17 +61,18 @@ final class AddGridClassesToContentListener
                     // code...
                     break;
                 case 'colStart':
-                    // vorhandene Klasse erweitern
                     $strBuffer = str_replace('ce_colStart', 'ce_colStart '.$strClasses, $strBuffer);
                     break;
                 default:
-                    // Umschließendes DIV hinzufügen
                     $strBuffer = '<div class="'.$strClasses.'">'.$strBuffer.'</div>';
                     break;
             }
+
+            if ($this->scopeMatcher->isBackendRequest($this->requestStack->getCurrentRequest())){
+                $strBuffer = '<div class="tl_gray tl_content_right">'.$strClasses.'</div>'.$strBuffer;
+            }
         }
 
-        // Rückgabe
         return $strBuffer;
     }
 }
--- src/EventListener/AddGridClassesToFormListener.php
+++ src/EventListener/AddGridClassesToFormListener.php
@@ -14,51 +14,30 @@ declare(strict_types=1);
 namespace ErdmannFreunde\ContaoGridBundle\EventListener;
 
 use Contao\Form;
+use Contao\StringUtil;
 use Contao\Widget;
 
 final class AddGridClassesToFormListener
 {
     public function onLoadFormField(Widget $objWidget, string $formId, array $data, Form $form)
     {
-        // Init
         $strClasses = '';
 
         // Bei diesen ContentElementen soll nichts verändert werden
         $arrWrongFields = ['rowStart', 'rowEnd', 'colEnd', 'html', 'fieldsetfsStop'];
 
-        // Abfrage, ob anzupassenden CEs und Klassen gesetzt wurden
         if (!\in_array($objWidget->type, $arrWrongFields, true) && (isset($objWidget->grid_columns) || isset($objWidget->grid_options))) {
             if ($objWidget->grid_columns) {
-                $env            = 'FE';
-                $strField       = 'grid_columns';
-                $arrGridClasses = unserialize($objWidget->grid_columns);
+                $arrGridClasses = StringUtil::deserialize($objWidget->grid_columns);
                 foreach ($arrGridClasses as $class) {
-                    // HOOK: create and manipulate grid classes
-                    if (isset($GLOBALS['TL_HOOKS']['manipulateGridClasses']) && \is_array($GLOBALS['TL_HOOKS']['manipulateGridClasses'])) {
-                        foreach ($GLOBALS['TL_HOOKS']['manipulateGridClasses'] as $callback) {
-                            $this->import($callback[0]);
-                            $class = $this->{$callback[0]}->{$callback[1]}($env, $strField, $class);
-                        }
-                    }
-
                     $strClasses .= $class.' ';
                 }
             }
 
             // Weitere Optionen Klassen auslesen und in String speichern
             if ($objWidget->grid_options) {
-                $env            = 'FE';
-                $strField       = 'grid_options';
-                $arrGridClasses = unserialize($objWidget->grid_options);
+                $arrGridClasses = StringUtil::deserialize($objWidget->grid_options);
                 foreach ($arrGridClasses as $class) {
-                    // HOOK: create and manipulate grid classes
-                    if (isset($GLOBALS['TL_HOOKS']['manipulateGridClasses']) && \is_array($GLOBALS['TL_HOOKS']['manipulateGridClasses'])) {
-                        foreach ($GLOBALS['TL_HOOKS']['manipulateGridClasses'] as $callback) {
-                            $this->import($callback[0]);
-                            $class = $this->{$callback[0]}->{$callback[1]}($env, $strField, $class);
-                        }
-                    }
-
                     $strClasses .= $class.' ';
                 }
             }
